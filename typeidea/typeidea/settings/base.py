0eecc1080276b7b58046e516b694e59269373792
lpdswing <lpdswing@qq.com>
2020-05-03 16:43:22 +0800

    完成app和model的创建

--- typeidea/typeidea/settings/base.py
+++ typeidea/typeidea/settings/base.py
@@ -30,12 +30,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
+    'config',
+    'comment'
 ]
 
 MIDDLEWARE = [
